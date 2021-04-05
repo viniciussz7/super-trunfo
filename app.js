c504a0fd28fd4573eb5df60157f4ed4e376a4c5f
viniciussz7 <79315396+viniciussz7@users.noreply.github.com>
2021-04-05 17:43:24 -0300

    incrementing the game

--- app.js
+++ app.js
@@ -28,30 +28,97 @@ var cartaGui = {
     }
 }
 
-var cartas = [cartaPaulo, cartaRafa, cartaGui]
+var cartaLol = {
+    nome: "Caitlyn",
+    imagem: "http://1.bp.blogspot.com/-K7CbqWc1-p0/VLc98v85s0I/AAAAAAAABqk/-ZB684VVHbg/s1600/Caitlyn_OriginalSkin.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 40,
+        magia: 10
+    }
+}
+
+var cartaNaruto = {
+    nome: "Naruto",
+    imagem: "https://conteudo.imguol.com.br/c/entretenimento/16/2017/06/27/naruto-1498593686428_v2_450x337.png",
+    atributos: {
+        ataque: 80,
+        defesa: 60,
+        magia: 100
+    }
+}
+
+var cartaHarry = {
+    nome: "Harry Potter",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/89ff10dd-aa41-4d17-ae8f-835281ebd3fd_49hp.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 50,
+        magia: 95
+    }
+}
+
+var cartaBatman = {
+    nome: "Batman",
+    imagem: "https://assets.b9.com.br/wp-content/uploads/2020/09/Batman-issue86-heder-1280x677.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 70,
+        magia: 0
+    }
+}
+
+var cartaMarvel = {
+    nome: "Capitã Marvel",
+    imagem: "https://cinepop.com.br/wp-content/uploads/2018/09/capitamarvel21.jpg",
+    atributos: {
+        ataque: 90,
+        defesa: 80,
+        magia: 0
+    }
+}
+
+
+var cartas = [cartaPaulo, cartaRafa, cartaGui, cartaLol, cartaNaruto, cartaHarry, cartaBatman, cartaMarvel]
 var cartaMaquina
 var cartaJogador
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQtdCartas()
+
+function atualizaQtdCartas() {
+    var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+    var html = "Quantidade de cartas no jogo: " + cartas.length
+    divQuantidadeCartas.innerHTML = html    
+}
+
+function atualizaPlacar() {
+    var divPlacar = document.getElementById('placar')
+    var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Máquina"
+    divPlacar.innerHTML = html 
+}
 
 function sortearCarta() {
-    var numeroCartaMaquina = parseInt(Math.random() * 3)
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
     cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
 
-    var numeroCartaJogador = parseInt(Math.random() * 3)
-    while (numeroCartaJogador == numeroCartaMaquina) {
-        numeroCartaJogador = parseInt(Math.random() * 3)
-    }
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    // while (numeroCartaJogador == numeroCartaMaquina) {
+    //     numeroCartaJogador = parseInt(Math.random() * 3)
+    // }
     cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
 
     document.getElementById('btnSortear').disabled = true
     document.getElementById('btnJogar').disabled = false
 
-    exibeCartaJogador()
-    
+    exibeCartaJogador()    
 }
 
-jogar()
-
-function exibeCartaJogador(){
+function exibeCartaJogador() {
     var divCartaJogador = document.getElementById("carta-jogador")
     var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
     divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
@@ -64,16 +131,6 @@ function exibeCartaJogador(){
     var html = "<div id='opcoes' class='carta-status'>"
     divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + "</div>"
 }
-
-
-// function exibirOpcoes() {
-//     var opcoes = document.getElementById('opcoes')
-//     var opcoesTexto = ""
-//     for (var atributo in cartaJogador.atributos) {
-//         opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo
-//     }
-//     opcoes.innerHTML = opcoesTexto
-// }
 
 function obtemAtributoSelecionado() {
     var radioAtributo = document.getElementsByName('atributo')
@@ -88,16 +145,39 @@ function jogar() {
     var atributoSelecionado = obtemAtributoSelecionado()
     var divResultado = document.getElementById("resultado")
     if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
-        htmlResultado = '<p class="resultado-final">Venceu!</p>'        
+        htmlResultado = '<p class="resultado-final">Venceu!</p>'
+        pontosJogador++       
     } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
         htmlResultado = '<p class="resultado-final">Perdeu!</p>'
+        pontosMaquina++
     } else {
         htmlResultado = '<p class="resultado-final">Empate!</p>'
     }
 
-    divResultado.innerHTML = htmlResultado
-    exibeCartaMaquina()
+    if (cartas.length == 0) {
+        alert("O jogo terminou!")
+        
+        if (pontosJogador > pontosMaquina) {
+            htmlResultado = '<p class="resultado-final">WINNER</p>'
+        } else if (pontosJogador < pontosMaquina) {
+            htmlResultado = '<p class="resultado-final">LOSER</p>'
+        } else {
+            htmlResultado = '<p class="resultado-final">Empatou. Tente novamente.</p>'
+        }
+              
+    } else {
+        document.getElementById('btnProximaRodada').disabled = false
+    }
 
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true
+    document.getElementById('btnProximaRodada').disabled = false
+    
+    atualizaPlacar()
+
+    exibeCartaMaquina()
+    
+    atualizaQtdCartas()
 }
 
 function exibeCartaMaquina(){
@@ -112,4 +192,17 @@ function exibeCartaMaquina(){
     }
     var html = "<div id='opcoes' class='carta-status'>"
     divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + "</div>"
+}
+
+function proximaRodada() {
+    var divCartas = document.getElementById('cartas')
+    divCartas.innerHTML =  `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+
+    document.getElementById('btnSortear').disabled = false
+    document.getElementById('btnJogar').disabled = true
+    document.getElementById('btnProximaRodada').disabled = true
+
+    var divResultado = document.getElementById('resultado')
+    divResultado.innerHTML = ""
+
 }
